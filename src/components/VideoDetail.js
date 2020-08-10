8d952e9db5b192dbd8e58b49bb95d8ee9b15d7f6
Sonu <sonu3660@gmail.com>
2020-08-11 01:23:22 +0530

    Add Video player by iframe tag

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,8 +5,14 @@ const VideoDetail = ({ video }) => {
    if (!video) {
       return <div>Loading...</div>;
    }
+
+   const videoSrc = `https://www.youtube.com/embed/${video.id.videoId} `;
+
    return (
       <div>
+         <div className="ui embed">
+            <iframe src={videoSrc} title="video player" />
+         </div>
          <div className="ui segment">
             <h4 className="ui header">{video.snippet.title}</h4>
             <p>{video.snippet.description}</p>
